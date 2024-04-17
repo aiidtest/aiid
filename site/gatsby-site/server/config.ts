821c29292941be1352f55735f19331043000a2e2
Cesar Varela <mail@cesarvarela.com>
2024-04-17 14:58:20 -0300

    Strongly typed environment config

---
+++ site/gatsby-site/server/config.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+import { createEnv } from './utils';
+
+const config = createEnv({
+    server: {
+        REALM_APP_ID: z.string().min(1),
+        REALM_API_APP_ID: z.string().min(1),
+        REALM_API_GROUP_ID: z.string().min(1),
+        REALM_API_PUBLIC_KEY: z.string().min(1),
+        REALM_API_PRIVATE_KEY: z.string().min(1),
+        MONGODB_CONNECTION_STRING: z.string().min(1),
+    },
+    runtimeEnv: process.env,
+});
+
+export default config;
