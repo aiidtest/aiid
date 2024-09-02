c38ac21e0cc26c35ceb02b71d2b0d7b558692ca5
Cesar Varela <mail@cesarvarela.com>
2024-09-02 16:57:24 -0300

    try test more

--- site/gatsby-site/playwright/e2e-full/api/parsenews.spec.ts
+++ site/gatsby-site/playwright/e2e-full/api/parsenews.spec.ts
@@ -1,6 +1,9 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('/api/parseNews endpoint', () => {
+
+    test.describe.configure({ retries: 4 });
+
     test('Should parse news', async ({ request, baseURL }) => {
         const newsUrl = 'https://incidentdatabase.ai/blog/improv-ai/';
         const apiUrl = `${baseURL}/api/parseNews?url=${encodeURIComponent(newsUrl)}`;
@@ -10,5 +13,5 @@ test.describe('/api/parseNews endpoint', () => {
 
         expect(response.status()).toBe(200);
         expect(responseBody.title).toBe('How to Understand Large Language Models through Improv');
-    });
+    },);
 });
